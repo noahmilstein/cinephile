6e738c7b28f7fc4de69bdbf6e3802ecaea292fda
Jun Oh <jsohpennstater@gmail.com>
2016-10-03 16:52:49 -0400

    Finshed second user stories

--- spec/features/01_prospective_user_spec.rb
+++ spec/features/01_prospective_user_spec.rb
@@ -11,12 +11,13 @@ require 'rails_helper'
 # [] User should be prompted to pick another username if its already exists in the new user form page with a message.
 # [] User should be prompted to use another email if that email is already associated with an account.
 # [] User should be prompted to choose a password that is six characters long with at least one number and one letter.
-# [] User cannot click on movie reviews if they are not signed and should be prompted either sign up as a new user or sign in as an exisiting user.
-# [] User cannot post items if they are not signed and should be prompted either sign up as a new user or sign in as an exisiting user.
+# [] User cannot click on movie reviews/post new review if they are not signed in and should be prompted either sign up as a new user or sign in as an exisiting user.
 
 feature "Prospective user" do
   context "as as prospective user" do
-    let! (:user1) { FactoryGirl.create(:user) }
+    user1 = User.new(first_name: "John", last_name: "Doe", username: "johndoe", email: "john@doe.com", password: "sixchr1",  newsletter: false)
+    user2 = User.new(first_name: "Jane", last_name: "Doe", username: "janedoe", email: "jane@doe.com", password: "sixchr1",  newsletter: false)
+    movie1 = Movie.create(title: "Citizen Kane", studio: "Studio", year: 1940, rating: "R", genre: "drama", cast: "Cast Member 1, Cast Member 2", director: "Director 1", screen_writer: "Screen Writer 1")
 
     scenario "sees sign up link" do
       visit "/"
@@ -37,7 +38,169 @@ feature "Prospective user" do
 
     scenario "user fills in sign-up form correctly and is redirected to the homepage"
       visit "/new"
-      fill_in "First Name", :with => ""
+      fill_in "First Name", :with => user1.first_name
+      fill_in "Last Name", :with => user1.last_name
+      fill_in "Username", :with => user1.username
+      fill_in "Email", :with => user1.email
+      fill_in "Enter Password", :with => user1.password
+      fill_in "Confirm Password", :with => user1.password
+      fill_in "Sign up for newsletter?", :with => false
+      click_button "Submit"
+      expect(page).to have_content("Welcome to Cinephile!")
+      expect(page).to have_current_path(movies_path, url: true)
+    end
 
+    scenario "user fills in sign-up form correctly and sees flash message confirming signup"
+      visit "/new"
+      fill_in "First Name", :with => user1.first_name
+      fill_in "Last Name", :with => user1.last_name
+      fill_in "Username", :with => user1.username
+      fill_in "Email", :with => user1.email
+      fill_in "Enter Password", :with => user1.password
+      fill_in "Confirm Password", :with => user1.password
+      fill_in "Sign up for newsletter?", :with => false
+      click_button "Submit"
+      expect(page).to have_content("Signed up successfully.")
+    end
+
+    scenario "user leaves first name blank and sees flash message with error"
+      visit "/new"
+      fill_in "Last Name", :with => user1.last_name
+      fill_in "Username", :with => user1.username
+      fill_in "Email", :with => user1.email
+      fill_in "Enter Password", :with => user1.password
+      fill_in "Confirm Password", :with => user1.password
+      fill_in "Sign up for newsletter?", :with => false
+      click_button "Submit"
+      expect(page).to have_current_path(new_user_path, url: true)
+      expect(page).to have_content("First name can't be blank")
+    end
+
+    scenario "user leaves last name blank and sees flash message with error"
+      visit "/new"
+      fill_in "First Name", :with => user1.first_name
+      fill_in "Username", :with => user1.username
+      fill_in "Email", :with => user1.email
+      fill_in "Enter Password", :with => user1.password
+      fill_in "Confirm Password", :with => user1.password
+      fill_in "Sign up for newsletter?", :with => false
+      click_button "Submit"
+      expect(page).to have_current_path(new_user_path, url: true)
+      expect(page).to have_content("Last name can't be blank")
+    end
+
+    scenario "user leaves username blank and sees flash message with error"
+      visit "/new"
+      fill_in "First Name", :with => user1.first_name
+      fill_in "Last Name", :with => user1.last_name
+      fill_in "Email", :with => user1.email
+      fill_in "Enter Password", :with => user1.password
+      fill_in "Confirm Password", :with => user1.password
+      fill_in "Sign up for newsletter?", :with => false
+      click_button "Submit"
+      expect(page).to have_current_path(new_user_path, url: true)
+      expect(page).to have_content("Username can't be blank")
+    end
+
+    scenario "user leaves email blank and sees flash message with error"
+      visit "/new"
+      fill_in "First Name", :with => user1.first_name
+      fill_in "Last Name", :with => user1.last_name
+      fill_in "Username", :with => user1.username
+      fill_in "Enter Password", :with => user1.password
+      fill_in "Confirm Password", :with => user1.password
+      fill_in "Sign up for newsletter?", :with => false
+      click_button "Submit"
+      expect(page).to have_current_path(new_user_path, url: true)
+      expect(page).to have_content("Email can't be blank")
+    end
+
+    scenario "user leaves Enter Password field blank and sees flash message with error"
+      visit "/new"
+      fill_in "First Name", :with => user1.first_name
+      fill_in "Last Name", :with => user1.last_name
+      fill_in "Email", :with => user1.email
+      fill_in "Username", :with => user1.username
+      fill_in "Confirm Password", :with => user1.password
+      fill_in "Sign up for newsletter?", :with => false
+      click_button "Submit"
+      expect(page).to have_current_path(new_user_path, url: true)
+      expect(page).to have_content("Password can't be blank")
+    end
+
+    scenario "user leaves Confirm Password field blank and sees flash message with error"
+      visit "/new"
+      fill_in "First Name", :with => user1.first_name
+      fill_in "Last Name", :with => user1.last_name
+      fill_in "Email", :with => user1.email
+      fill_in "Username", :with => user1.username
+      fill_in "Enter Password", :with => user1.password
+      fill_in "Sign up for newsletter?", :with => false
+      click_button "Submit"
+      expect(page).to have_current_path(new_user_path, url: true)
+      expect(page).to have_content("Password can't be blank")
+    end
+
+    scenario "user leaves newsletter field blank and sees flash message with error"
+      visit "/new"
+      fill_in "First Name", :with => user1.first_name
+      fill_in "Last Name", :with => user1.last_name
+      fill_in "Email", :with => user1.email
+      fill_in "Username", :with => user1.username
+      fill_in "Enter Password", :with => user1.password
+      fill_in "Confirm Password", :with => user1.password
+      click_button "Submit"
+      expect(page).to have_current_path(new_user_path, url: true)
+      expect(page).to have_content("Newsletter can't be blank")
+    end
+
+    scenario "user selects username that is taken and is prompted to choose another"
+      visit "/new"
+      fill_in "First Name", :with => user2.first_name
+      fill_in "Last Name", :with => user2.last_name
+      fill_in "Username", :with => user1.username
+      fill_in "Email", :with => user2.email
+      fill_in "Enter Password", :with => user2.password
+      fill_in "Confirm Password", :with => user2.password
+      fill_in "Sign up for newsletter?", :with => false
+      click_button "Submit"
+      expect(page).to have_current_path(new_user_path, url: true)
+      expect(page).to have_content("Please choose another username.")
+    end
+
+    scenario "user inputs email that is already used and is prompted to choose another"
+      visit "/new"
+      fill_in "First Name", :with => user2.first_name
+      fill_in "Last Name", :with => user2.last_name
+      fill_in "Username", :with => user2.username
+      fill_in "Email", :with => user1.email
+      fill_in "Enter Password", :with => user2.password
+      fill_in "Confirm Password", :with => user2.password
+      fill_in "Sign up for newsletter?", :with => false
+      click_button "Submit"
+      expect(page).to have_current_path(new_user_path, url: true)
+      expect(page).to have_content("Please choose another username.")
+    end
+
+    scenario "user inputs malformed password and is prompted to choose another"
+      visit "/new"
+      fill_in "First Name", :with => user2.first_name
+      fill_in "Last Name", :with => user2.last_name
+      fill_in "Username", :with => user2.username
+      fill_in "Email", :with => user2.email
+      fill_in "Enter Password", :with => "abc"
+      fill_in "Confirm Password", :with => "abc"
+      fill_in "Sign up for newsletter?", :with => false
+      click_button "Submit"
+      expect(page).to have_current_path(new_user_path, url: true)
+      expect(page).to have_content("Please choose a password of at least 6 characters with at least one number and one letter.")
+    end
+
+    scenario "user attempts to navigate to movie show page/make a review and is redirected to homepage with message to sign in"
+      visit "/movies"
+      click_link (movie1.title)
+      expect(page).to have_current_path(movies_path, url: true)
+      expect(page).to have_content("Please sign in or sign up in order to view this movie and its reviews")
+    end
   end
 end
--- spec/features/02_unauthenticated_user_spec.rb
+++ spec/features/02_unauthenticated_user_spec.rb
@@ -1,3 +1,5 @@
+require 'rails_helper'
+
 # As an unauthenticated user
 # I want to sign in
 # So that I can post items and review them
@@ -9,3 +11,86 @@
 # [] Unauthenticated user correctly fills in the form for the sign in page and is redirected to the home page, so that you can post and review movie items.
 # [] Unauthenticated user incorrectly fills in the form for the sign in page and is redirected to the sign in page where the site tells the user to try again because username is incorrect.
 # [] Unauthenticated user correctly fills in the username, but types in incorrect password. The user is then redirected to the sign in page where the site tells the user to provide the correct password.
+# [] Unauthenticated user signs in and clicks on a movie to make a review
+
+
+feature "Unauthenticated user" do
+  context "as as unauthenticated user" do
+    user1 = User.create(first_name: "John", last_name: "Doe", username: "johndoe", email: "john@doe.com", password: "abcdef1",  newsletter: false)
+    movie1 = Movie.create(title: "Citizen Kane", studio: "Studio", year: 1940, rating: "R", genre: "drama", cast: "Cast Member 1, Cast Member 2", director: "Director 1", screen_writer: "Screen Writer 1")
+
+    scenario "sees sign in link" do
+      visit "/"
+      expect(page).to have_link("Sign In")
+    end
+
+    scenario "click sign up link and see fields to complete for registration" do
+      visit "/"
+      click_link("Sign In")
+      expect(page).to have_content("Username")
+      expect(page).to have_content("Password")
+    end
+
+    scenario "user fills in sign-in form correctly and is redirected to the homepage"
+      visit "/"
+      click_link("Sign In")
+      fill_in "Username", :with => user1.username
+      fill_in "Password", :with => user1.password
+      click_button "Sign In"
+      expect(page).to have_content("Welcome to Cinephile!")
+      expect(page).to have_current_path(movies_path, url: true)
+    end
+
+    scenario "user fills in sign-in form correctly and sees flash message confirming signin"
+      visit "/"
+      click_link("Sign In")
+      fill_in "Username", :with => user1.username
+      fill_in "Password", :with => user1.password
+      click_button "Sign In"
+      expect(page).to have_content("Welcome to Cinephile!")
+      expect(page).to have_current_path(movies_path, url: true)
+      expect(page).to have_content("Signed in successfully!")
+    end
+
+    scenario "user fills in sign-in form incorrectly and sees flash message with error"
+      visit "/"
+      click_link("Sign In")
+      fill_in "Username", :with => "incorrect"
+      fill_in "Password", :with => user1.password
+      click_button "Sign In"
+      expect(page).to have_current_path(sign_in_path, url: true) ##check path later
+      expect(page).to have_content("Username invalid, Try again!")
+    end
+
+    scenario "user fills in sign-up form incorrectly and sees flash message with error"
+      visit "/"
+      click_link("Sign In")
+      fill_in "Username", :with => user1.username
+      fill_in "Password", :with => "incorrect"
+      click_button "Sign In"
+      expect(page).to have_current_path(sign_in_path, url: true) ##check path later
+      expect(page).to have_content("Password invalid, Try again!")
+    end
+
+    scenario "user fills in sign-in form correctly, sees flash message confirming signin, and successfully clicks on a movie_link."
+      visit "/"
+      click_link("Sign In")
+      fill_in "Username", :with => user1.username
+      fill_in "Password", :with => user1.password
+      click_button "Sign In"
+      expect(page).to have_content("Welcome to Cinephile!")
+      expect(page).to have_current_path(movies_path, url: true)
+      expect(page).to have_content("Signed in successfully!")
+      click_link("Citizen Kane")
+      expect(page).to have_content("Citizen Kane")
+      expect(page).to have_content("Studio")
+      expect(page).to have_content(1940)
+      expect(page).to have_content("R")
+      expect(page).to have_content("drama")
+      expect(page).to have_content("Cast Member 1, Cast Member 2")
+      expect(page).to have_content("Director 1")
+      expect(page).to have_content("Screen Writer 1")
+    end
+
+  end
+end
