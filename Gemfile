c0755cfb2e6f51b36d415a8f2844988ddcf42ae4
noahmilstein <casanoah@gmail.com>
2016-10-03 15:34:06 -0400

    initial devise set up

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
 gem 'listen'
+gem 'devise'
 group :development, :test do
   gem 'capybara'
   gem 'factory_girl_rails'
@@ -13,6 +14,7 @@ group :development, :test do
   gem 'pry-rails'
   gem 'shoulda'
   gem 'valid_attribute'
+  gem 'httparty'
 end
 
 group :test do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# fields_to_insert = [ "DVD_Title", "Studio", "Rating", "Year", "Genre" ]
+#
+# rows_to_insert = []
+#
+# CSV.foreach("./db/movies.csv", headers:true) do |row|
+#   row_to_insert = row.to_hash.select { |k,v| fields_to_insert.include?(k) }
+#   rows_to_insert << row_to_insert
+# end
+#
+# rows_to_insert.each do |movie|
+#   Movie.create(title: movie["DVD_Title"], studio: movie["Studio"], year: movie["Year"], rating: movie["Rating"], genre: movie["Genre"])
+# end
