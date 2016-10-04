d5fc952ed26e8bb8efdda6abfd637c61fcccdff8
jeffreyfriedman <jeff@eurodollaryen.com>
2016-10-04 09:44:23 -0400

    removed user story and acceptance criteria comments

--- spec/features/01_prospective_user_spec.rb
+++ spec/features/01_prospective_user_spec.rb
@@ -1,18 +1,5 @@
 require 'rails_helper'
 
-# As a prospective user
-# I want to create an account
-# So that I can post items and review them
-#
-# Accpetance Criteria
-# [] User clicks sign up link and fills in the form for username, password, email, and neccessary fields for new user.
-# [] User clicks sign up and fills in the form for email as username and password correctly and the rest of the form. User should be redirected to the home page with a message that welcomes them to the site "Welcome to Cinephile!".
-# [] User clicks sign up and fills in the form incorrectly. User should be redirected to the same sign up page and be asked to fill in all the required fields with a message.
-# [] User should be prompted to pick another username if its already exists in the new user form page with a message.
-# [] User should be prompted to use another email if that email is already associated with an account.
-# [] User should be prompted to choose a password that is six characters long with at least one number and one letter.
-# [] User cannot click on movie reviews/post new review if they are not signed in and should be prompted either sign up as a new user or sign in as an exisiting user.
-
 feature "Prospective user" do
   context "as prospective user" do
     user1 = User.new(first_name: "John", last_name: "Doe", username: "johndoe", email: "john@doe.com", password: "sixchr1",  newsletter: true)
--- spec/features/02_unauthenticated_user_spec.rb
+++ spec/features/02_unauthenticated_user_spec.rb
@@ -1,19 +1,5 @@
 require 'rails_helper'
 
-# As an unauthenticated user
-# I want to sign in
-# So that I can post items and review them
-
-# Accpetance Criteria
-# [] Unauthenticated user visits the site and sees an option to log in.
-# [] Unauthenticated user visits the site and clicks on the link to sign in.
-# [] Unauthenticated user is directed to sign in page where the user can fill in username and password.
-# [] Unauthenticated user correctly fills in the form for the sign in page and is redirected to the home page, so that you can post and review movie items.
-# [] Unauthenticated user incorrectly fills in the form for the sign in page and is redirected to the sign in page where the site tells the user to try again because username is incorrect.
-# [] Unauthenticated user correctly fills in the username, but types in incorrect password. The user is then redirected to the sign in page where the site tells the user to provide the correct password.
-# [] Unauthenticated user signs in and clicks on a movie to make a review
-
-
 feature "Unauthenticated user" do
   context "as as unauthenticated user" do
     user1 = User.create(first_name: "John", last_name: "Doe", username: "johndoe", email: "john@doe.com", password: "abcdef1",  newsletter: true)
