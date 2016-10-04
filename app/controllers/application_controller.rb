746528f3a944309cd1f9563fc899d5204d65b4bb
jeffreyfriedman <jeff@eurodollaryen.com>
2016-10-04 10:01:23 -0400

    fixed hound errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,16 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:first_name, :last_name, :username, :email, :password, :password_confirmation, :newsletter, :remember_me]
+    added_attrs = [
+      :first_name,
+      :last_name,
+      :username,
+      :email,
+      :password,
+      :password_confirmation,
+      :newsletter,
+      :remember_me
+    ]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
