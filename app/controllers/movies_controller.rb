23b1229cf222596888d9c92537923ef6d2510a41
Jun Oh <jsohpennstater@gmail.com>
2016-10-04 14:50:33 -0400

    finished Movies_Create

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,6 +9,20 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+   @movie = Movie.new(movie_params)
+     if @movie.save
+       redirect_to movie_path(@movie)
+       flash[:notice] = "You successfully added a movie"
+     else
+       render :new
+     end
+  end
+
   protected
 
   def authorize_user
@@ -17,5 +31,19 @@ class MoviesController < ApplicationController
       " order to view this movie and its reviews"
       redirect_to root_path
     end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title,
+    :studio,
+    :year,
+    :rating,
+    :genre,
+    :cast,
+    :director,
+    :screen_writer
+    )
   end
 end
