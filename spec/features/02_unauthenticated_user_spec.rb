41ec0d7ab8ae486b9ceb09f5b9e3bca7011482cb
jeffreyfriedman <jeff@eurodollaryen.com>
2016-10-04 10:31:10 -0400

    hound configuration

--- spec/features/02_unauthenticated_user_spec.rb
+++ spec/features/02_unauthenticated_user_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 feature "Unauthenticated user" do
   context "as as unauthenticated user" do
--- spec/features/03_authenticated_user_spec.rb
+++ spec/features/03_authenticated_user_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 feature "Authenticated user" do
   context "as as Authenticated user" do
