89225e457ba30217182a778f59606260647ec30e
jeffreyfriedman <jeff@eurodollaryen.com>
2016-10-04 09:55:05 -0400

    fixed hound errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,19 +6,4 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
-  def create
-    # first_name = params[:first_name]
-    # last_name = params[:last_name]
-    # username = params[:username]
-    # email = params[:email]
-    # newsletter = params[:newsletter]
-    # password = params[:password]
-    # all_params = [ first_name, last_name, username, email, newsletter, password ]
-    # unless params.any? { |x| x.nil? }
-    # else
-    #   render :'devise/registrations/new'
-    # end
-    #
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,4 @@ Rails.application.routes.draw do
 
   resources :movies
   resources :users
-
 end
-
-
-# devise_scope :user do
-#   authenticated :user do
-#     root "movies#index", as: :authenticated_root
-#   end
-#   unauthenticated :user do
-#     root "devise/registrations#new", as: :unauthenticated_root
-#   end
-# end
