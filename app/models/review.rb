2df517b8b111c72f564adc41997a4e6c237614b5
noahmilstein <casanoah@gmail.com>
2016-10-05 16:34:02 -0400

    final update create reviews branch

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,11 +1,12 @@
 class Review < ActiveRecord::Base
   validates :title, presence: true
   validates :rating, numericality: { integer_only: true }
-  validates :body, presence: true, length: { minimum: 50, message: "Body must be at least 50 characters" }
+  validates :body, presence: true, length: { minimum: 50, message:
+    "Body must be at least 50 characters" }
 
   validate :rating_range
   def rating_range
-    unless (1..5).include?(self.rating.to_i)
+    unless (1..5).cover?(self.rating.to_i)
       errors.add(:rating_range, "Rating must be 1 - 5")
     end
   end
--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :review do
     sequence(:title) { |n| "#{n}title" }
-    sequence(:body) { |n| "#{n} This is a long body description, at least 50 characters long"  }
+    sequence(:body) { |n| "#{n} This is a long body description,"\
+    "at least 50 characters long"  }
     rating "4"
     movie
     user
