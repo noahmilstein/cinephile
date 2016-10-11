97d22e12f7dc0a30a5926b444866aee02ab881fd
Michael Walsh <michael.c.walsh1@gmail.com>
2016-10-11 11:41:17 -0400

    fixed hound conflict

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
       provider: "AWS",
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: 'us-west-2'
+      region: "us-west-2"
     }
-    config.fog_directory  = 'mcwcinephile'
+    config.fog_directory  = "mcwcinephile"
   end
 end
