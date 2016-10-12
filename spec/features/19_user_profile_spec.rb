fa3b06b9f5e46aba66bec8933cf31f3fe909dd68
noahmilstein <casanoah@gmail.com>
2016-10-12 15:18:20 -0400

    WIP

--- spec/features/19_user_profile_spec.rb
+++ spec/features/19_user_profile_spec.rb
@@ -1,5 +1,6 @@
 require "rails_helper"
 
+
 feature "User profile" do
   let!(:user1) { FactoryGirl.create(:user) }
   let!(:user2) { FactoryGirl.create(:user) }
@@ -23,4 +24,14 @@ feature "User profile" do
     expect(page).to have_link(review1.title)
     expect(page).to_not have_content(review2.title)
   end
+
+  scenario "Nav bar is up to date has user profile" do
+    user profile
+    Nav bar ‘logged in as <username> <user image thumbnail>
+  end
+
+  scenario "user profile has edit profile" do
+  end
+  Review displays OP username
+
 end
