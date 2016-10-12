0957c9c47dde14aa28188449b176b55d5e7271e2
Michael Walsh <michael.c.walsh1@gmail.com>
2016-10-12 11:55:41 -0400

    refactored tests

--- spec/features/13_user_adds_profile_photo_spec.rb
+++ spec/features/13_user_adds_profile_photo_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 feature "profile photo" do
-  let!(:user) { FactoryGirl.create(:user) }
+  let(:user) { User.new(email: "email@website.com", username: "username", first_name: "firstname", last_name: "lastname", password: "password") }
 
   scenario "new user sees avatar upload option on sign up page" do
     visit root_path
@@ -22,22 +22,33 @@ feature "profile photo" do
     fill_in "Confirm Password", with: user.password
     attach_file "Add Avatar", "#{Rails.root}/spec/support/images/twitter-egg-icon.jpg"
     click_button "Submit"
-    visit user_path(user.id)
+    click_link "My Profile"
 
     expect(page).to have_content(user.username)
     expect(page).to have_xpath("//img")
   end
 
   scenario "user edits a profile photo" do
-    user_sign_in(user)
-    click_link "Edit registration"
+    visit root_path
+    click_link "Sign Up"
+
+    fill_in "Email", with: user.email
+    fill_in "Username", with: user.username
+    fill_in "First Name", with: user.first_name
+    fill_in "Last Name", with: user.last_name
+    fill_in "Enter Password", with: user.password
+    fill_in "Confirm Password", with: user.password
+    attach_file "Add Avatar", "#{Rails.root}/spec/support/images/twitter-egg-icon.jpg"
+    click_button "Submit"
+    click_link "My Profile"
+    click_link "Edit profile"
 
     attach_file "Edit Avatar", "#{Rails.root}/spec/support/images/homer.jpg"
     fill_in "Enter Password", with: user.password
     fill_in "Confirm Password", with: user.password
     fill_in "Current Password", with: user.password
     click_button "Update"
-    visit user_path(user.id)
+    click_link "My Profile"
 
     expect(page).to have_content(user.username)
     expect(page).to have_xpath("//img")
