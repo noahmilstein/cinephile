d19fa3260ac32c07251b8d68cc4e267149c85b95
Michael Walsh <michael.c.walsh1@gmail.com>
2016-10-12 22:16:26 -0400

    re-factored test

---
+++ spec/features/21_add_movie_poster_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+feature "movie poster" do
+  let!(:user) { FactoryGirl.create(:user) }
+
+  before do
+    user_sign_in(user)
+  end
+
+  scenario "user sees poster upload option on new movie form" do
+    click_link "Add New Movie"
+
+    expect(page).to have_content("Add Poster")
+  end
+
+  scenario "user uploads a movie poster" do
+    movie = Movie.new(title: "movietitle", studio: "studio", year: "1000", rating: "G", genre: "genre", cast_member: "cast", director: "director", screen_writer: "screen writer")
+    click_link "Add New Movie"
+
+    fill_in "Title", with: movie.title
+    fill_in "Studio", with: movie.studio
+    fill_in "Year", with: movie.year
+    fill_in "Genre", with: movie.genre
+    fill_in "Cast", with: movie.cast_member
+    fill_in "Director", with: movie.director
+    fill_in "Rating", with: movie.rating
+    fill_in "Screen Writer", with: movie.screen_writer
+    attach_file "Add Poster", "#{Rails.root}/spec/support/images/2001.jpg"
+    click_button "Submit"
+
+    expect(page).to have_content(movie.title)
+    expect(page).to have_xpath("//img")
+  end
+end
